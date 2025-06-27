74c1d39ab374d8780242c7e3cba7ce419dd1f9b7
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-06-28 03:00:29 +0530

    updated AppointmentModal

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -11,7 +11,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Appointment } from "@/types/appwrite.types";
+import { Appointment } from "@/types/supabase.types";
 
 import { AppointmentForm } from "./forms/AppointmentForm";
 
